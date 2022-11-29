76cfd6a5f9c240cacbfeab85e3b8bd9f85c84cdc
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-11-29 20:56:27 +0100

    Login
    The login logic

--- TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -10,9 +10,15 @@ namespace TravelRecordApp
             InitializeComponent();
         }
 
-        private void loginButton_Clicked(object sender, EventArgs e)
+        private void LoginButton_Clicked(object sender, EventArgs e)
         {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
 
+            if(isEmailEmpty || isPasswordEmpty )
+            {
+
+            }
         }
     }
 }
