993c6df4c33e32be34399ee58fc6f07ed8277396
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-12-30 14:54:37 +0100

    PostDetailPage  - working on

---
+++ TravelRecordApp/TravelRecordApp/PostDetailPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PostDetailPage : ContentPage
+	{
+        Post selectedPost;
+
+		public PostDetailPage (Post selectedPost)
+		{
+			InitializeComponent ();
+            this.selectedPost = selectedPost;
+            experienceEntry.Text = selectedPost.Experience;
+		}
+
+        private void UpdateButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
