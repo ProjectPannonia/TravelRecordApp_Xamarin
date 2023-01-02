4fb800ed18c372268af39a9e75648eb7c7dad907
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-01-02 17:18:15 +0100

    Newtonsoft dependency added

---
+++ TravelRecordApp/TravelRecordApp/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecordApp.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = "JP2OC3VRHSONAF1FMDGNI0ZYCCQ21XUMW2ME3DR3SVM43YN5";
+        public const string CLIENT_SECRET = "APJWYLXNPIZAD0AEGFT0SSXOS1PRYRCDK23EAIBJPBUJI5Z2";
+    }
+}
