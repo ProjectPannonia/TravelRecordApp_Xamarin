cdfc53c91e60e14c2b6242f0c9dbfbe7be42a46c
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-01-02 11:30:21 +0100

    Update position

--- TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using System;
+using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace TravelRecordApp
@@ -8,6 +12,8 @@ namespace TravelRecordApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
+        IGeolocator locator = CrossGeolocator.Current;
+
         public MapPage()
         {
             InitializeComponent();
@@ -19,6 +25,14 @@ namespace TravelRecordApp
             GetLocation();
         }
 
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            locator.StopListeningAsync();
+
+        }
+
         private async void GetLocation()
         {
             var status = await CheckAndRequestLocationPermission();
@@ -26,8 +40,28 @@ namespace TravelRecordApp
             if(status == PermissionStatus.Granted)
             {
                 var location = await Geolocation.GetLocationAsync();
+
+                //var locator = CrossGeolocator.Current;
+                locator.PositionChanged += Locator_PositionChanged;
+                await locator.StartListeningAsync(new TimeSpan(0,1,0), 100);
+
                 locationsMap.IsShowingUser = true;
+
+                CenterMap(location.Latitude, location.Longitude);
             }
+        }
+
+        private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            CenterMap(e.Position.Latitude, e.Position.Longitude);
+
+        }
+
+        private void CenterMap(double latitude, double longitude)
+        {
+            Xamarin.Forms.Maps.Position center = new Xamarin.Forms.Maps.Position(latitude, longitude);
+            MapSpan span = new MapSpan(center, 1, 1);
+            locationsMap.MoveToRegion(span);
         }
 
         private async Task<PermissionStatus> CheckAndRequestLocationPermission()
